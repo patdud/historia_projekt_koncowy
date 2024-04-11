62b7e36bb4ec83a4b26aa40807e752f5e8a651f2
ASowa <antonisiwiorek@gmail.com>
2024-04-11 20:54:39 +0200

    Create models: answer, answer_type

--- viewer/models.py
+++ viewer/models.py
@@ -1,3 +1,29 @@
-from django.db import models
+from django.db.models import (
+    Model,
+    CharField,
+    DateField,
+    DateTimeField,
+    ForeignKey,
+    IntegerField,
+    TextField,
+    BooleanField,
+    CASCADE,
+    DO_NOTHING
+)
 
 
+class AnswerType(Model):
+    type_name = CharField(max_length=255)
+
+    def __str__(self):
+        return self.type_name
+
+
+class Answer(Model):
+    question_id = ForeignKey(Question)
+    content = CharField(max_length=255)
+    type_id = ForeignKey(AnswerType, on_delete=DO_NOTHING)
+    flag = BooleanField(default=False)
+
+    def __str__(self):
+        return self.content
