65e6b68a4d62140cd1e6bfae246924c4340eee91
patdud <patrykdudek.dudekpatryk@gmail.com>
2024-04-15 17:13:36 +0200

    15.04 added Category, Article and UserCategory models

--- viewer/models.py
+++ viewer/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db.models import (
+    Model,
+    CharField,
+    DateField,
+    DateTimeField,
+    ForeignKey,
+    IntegerField,
+    TextField,
+    BooleanField,
+    CASCADE,
+    DO_NOTHING,
+    SET_NULL,
+)
+
+from django.contrib.auth.models import User
+
+class Category(Model):
+    name = CharField(max_length=100, null=True)
+
+class Article(Model):
+    title = CharField(max_length=100, null=True)
+    content = TextField()
+    category_id = ForeignKey(Category, on_delete=DO_NOTHING)
+    image = CharField(max_length=200, null=True)
+
+class UserCategory(Model):
+    user_id = ForeignKey(User, on_delete=DO_NOTHING)
+    category_id = ForeignKey(Category, on_delete=DO_NOTHING())
+    points = IntegerField()
