7f734091590d10d6a0be72ac4f4bdef303a0228b
Greg <grzegorz.wisiorowski@gmail.com>
2024-04-15 17:43:05 +0200

    added Level and Question models

--- viewer/models.py
+++ viewer/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Level(Model):
+    name = CharField(max_length=10)
+    threshold = IntegerField()
+
+
+    def __str__(self):
+        return self.title
+
+
+class Question(Model):
+    contents = TextField()
+    category_id = ForeignKey(Category)
+    score = IntegerField()
+    level = ForeignKey(Level)
+
+    def __str__(self):
+        return f"{self.contents}"
