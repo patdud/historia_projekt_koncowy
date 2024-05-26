e855e1b38c33845e154230e61e0073b834ac4ee2
ASowa <antonisiwiorek@gmail.com>
2024-05-26 16:54:25 +0200

    import SECRET_KEY from other file

--- magi_vita/settings.py
+++ magi_vita/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from secret_key import key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wtu^ogwj5vhblhrb)r)on@llh)be7kkw^ba317o2-p84b1yqvd'
+SECRET_KEY = key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
